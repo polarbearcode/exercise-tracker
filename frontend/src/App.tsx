870a89de7cf47b2248a02786d99909ade34e672a
Benny Chen <benny.jw.chen@gmail.com>
2025-10-28 11:24:33 -0700

    added home element

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,13 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import TestPage from "./TestPage";
+import Home from "./Home";
 
 export default function App() {
   return (
     <>
       <Routes>
-        <Route
-          path="/"
-          element={<div>Welcome to the Exercise Tracker App</div>}
-        />
+        <Route path="/" element={<Home />} />
         <Route path="/test" element={<TestPage />} />
       </Routes>
     </>
