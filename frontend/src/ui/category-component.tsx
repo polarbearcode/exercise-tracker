c31aa54ec4211415025d0baaff08b82b23f0b063
Benny Chen <benny.jw.chen@gmail.com>
2025-10-31 16:05:33 -0700

    added undo button to categories

--- frontend/src/ui/category-component.tsx
+++ frontend/src/ui/category-component.tsx
@@ -15,20 +15,36 @@ export default function ExerciseCategoryComponent({
     category.exercises
   );
 
+  // track deleted exercises in the exercise category
+  const [deletedExercises, setDeletedExercises] = useState<Exercise[]>([]);
+
   /**
    * Handles when the delete button is pressed on an exercise.
    * Removes the exercise from the list displayed in this category.
+   * Puts it to top of the deleted exercise stack.
    */
   function handleExerciseDeleteButton(exercise: Exercise) {
     setCategoryExerciseList((prev) =>
       prev.filter((ex) => ex.name !== exercise.name)
     );
+
+    setDeletedExercises((prev) => [exercise, ...prev]);
+  }
+
+  // Handle undoing deletion of an exercise.
+  function handleUndoDelete() {
+    if (deletedExercises.length === 0) {
+      return;
+    }
+    setCategoryExerciseList((prev) => [...prev, deletedExercises[0]]);
+    setDeletedExercises((prev) => prev.slice(1));
   }
 
   return (
     <>
       <div key={"div-" + category.name}>
         <h2>{category.name}</h2>
+        <button onClick={handleUndoDelete}>Undo</button>
         {categoryExerciseList.map((exercise: Exercise) => {
           return (
             <ExerciseComponent
