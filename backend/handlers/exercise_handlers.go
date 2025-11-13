2d20285e023b3cd1d8e989c2df1933db06bb1f00
Benny Chen <benny.jw.chen@gmail.com>
2025-11-13 10:21:01 -0800

    changed delete to use string

--- backend/handlers/exercise_handlers.go
+++ backend/handlers/exercise_handlers.go
@@ -5,6 +5,8 @@ import (
 	"exercise-tracker/models"
 	"exercise-tracker/repository"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func GetExercises(w http.ResponseWriter, r *http.Request) {
@@ -38,20 +40,21 @@ func AddExercise(w http.ResponseWriter, r *http.Request) {
 }
 
 func DeleteExercise(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
 
-	var ex models.Exercise
-    if err := json.NewDecoder(r.Body).Decode(&ex); err != nil {
-        http.Error(w, "Invalid JSON", http.StatusBadRequest)
-        return
-    }
+	exerciseName := chi.URLParam(r, "name")
+	
+	if exerciseName == "" {
+		http.Error(w, "Missing exercise name", http.StatusBadRequest)
+		return
+	}
+	
 
-    if err := repository.DeleteExercise(ex); err != nil {
+    if err := repository.DeleteExercise(exerciseName); err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
 
     w.WriteHeader(http.StatusCreated)
-    json.NewEncoder(w).Encode(map[string]string{"message": "Exercise added successfully"})
+    json.NewEncoder(w).Encode(map[string]string{"message": "Exercise deleted"})
 
 }
