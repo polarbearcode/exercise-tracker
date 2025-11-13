d9331995025a78ccdf88f8d208df0b17ff7031f1
Benny Chen <benny.jw.chen@gmail.com>
2025-11-13 10:51:44 -0800

    wrote better logs

--- backend/handlers/exercise_handlers.go
+++ backend/handlers/exercise_handlers.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"exercise-tracker/models"
 	"exercise-tracker/repository"
+	"exercise-tracker/utils"
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -14,6 +16,7 @@ func GetExercises(w http.ResponseWriter, r *http.Request) {
 
     exercises, err := repository.GetAllExercises()
     if err != nil {
+		utils.LogErrorJSON(w, r, http.StatusInternalServerError, err, "Error fetching all exercises")
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
@@ -26,11 +29,13 @@ func AddExercise(w http.ResponseWriter, r *http.Request) {
 
     var ex models.Exercise
     if err := json.NewDecoder(r.Body).Decode(&ex); err != nil {
-        http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		utils.LogErrorJSON(w, r, http.StatusBadRequest, err, "Invalid JSON to add exercise")
+        http.Error(w, "Invalid JSON to add exercise", http.StatusBadRequest)
         return
     }
 
     if err := repository.AddExercise(ex); err != nil {
+		utils.LogErrorJSON(w, r, http.StatusInternalServerError, err, fmt.Sprintf("Error adding exercise: %s", ex.Name))
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
@@ -44,17 +49,19 @@ func DeleteExercise(w http.ResponseWriter, r *http.Request) {
 	exerciseName := chi.URLParam(r, "name")
 	
 	if exerciseName == "" {
+		utils.LogErrorJSON(w, r, http.StatusBadRequest, nil, "Missing exercise name")
 		http.Error(w, "Missing exercise name", http.StatusBadRequest)
 		return
 	}
 	
 
     if err := repository.DeleteExercise(exerciseName); err != nil {
+		utils.LogErrorJSON(w, r, http.StatusInternalServerError, err, fmt.Sprintf("Exercise %s could not be deleted", exerciseName))
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
 
     w.WriteHeader(http.StatusCreated)
-    json.NewEncoder(w).Encode(map[string]string{"message": "Exercise deleted"})
+    json.NewEncoder(w).Encode(map[string]string{"message": fmt.Sprintf("Exercise %s deleted", exerciseName)})
 
 }
