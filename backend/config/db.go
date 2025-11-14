2c0741191f3e0b954fb138d5d34d05d5d5447908
Benny Chen <benny.jw.chen@gmail.com>
2025-11-13 17:56:00 -0800

    added loading env

--- backend/config/db.go
+++ backend/config/db.go
@@ -4,12 +4,19 @@ import (
 	"log"
 	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/supabase-community/supabase-go"
 )
 
 var Supabase *supabase.Client
 
 func InitDB() {
+
+	err := godotenv.Load()
+    if err != nil {
+        log.Println("No .env file found — using system environment variables")
+    }
+	
     url := os.Getenv("DB_URL")
     key := os.Getenv("DB_API_KEY")
 
