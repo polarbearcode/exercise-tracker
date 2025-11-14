8dd9b5bbac375fea776bd24ed82ca76375224fd6
Benny Chen <benny.jw.chen@gmail.com>
2025-11-13 17:56:23 -0800

    added db init

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"exercise-tracker/config"
 	"exercise-tracker/routes"
 	"net/http"
 )
 
 func main() {
+	config.InitDB()
 	r := routes.NewRouter()
 	http.ListenAndServe(":8080", r)
 }
