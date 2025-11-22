7f5e85143f269fa3195a667e7550c39447c85221
Benny Chen <benny.jw.chen@gmail.com>
2025-11-21 22:55:57 -0800

    added checker

--- backend/utils/utils.go
+++ backend/utils/utils.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"exercise-tracker/models"
 	"log"
 	"net/http"
 )
@@ -30,4 +31,23 @@ func LogErrorJSON(w http.ResponseWriter, r *http.Request, status int, err error,
 	log.Printf(`{"status":%d,"method":"%s","path":"%s","message":"%s","error":"%v"}`, 
 		status, r.Method, r.URL.Path, msg, err)
 	http.Error(w, msg, status)
+}
+
+func logErrorInvalidExercise(w http.ResponseWriter, r *http.Request, msg string) {
+	log.Printf(`{"status":400,"method":"%s","path":"%s","message":"%s"}`, 
+		r.Method, r.URL.Path, msg)
+	http.Error(w, msg, http.StatusBadRequest)
+}
+
+func CheckExerciseFields(w http.ResponseWriter, r *http.Request, ex models.Exercise) {
+	if ex.Name == "" {
+		logErrorInvalidExercise(w, r, "Exercise name is required")
+		return 
+	}
+
+	if ex.Count < 0 {
+	logErrorInvalidExercise(w, r, "Exercise count cannot be negative")
+		return 
+	}
+
 }
