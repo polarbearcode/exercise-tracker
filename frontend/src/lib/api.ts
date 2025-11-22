509a6a60c69143f2b1ef0bada75b33efbf370c10
Benny Chen <benny.jw.chen@gmail.com>
2025-11-21 22:50:55 -0800

    functions for database

---
+++ frontend/src/lib/api.ts
@@ -0,0 +1,34 @@
+import type { Exercise } from "./definitions";
+
+const BASE_URL = import.meta.env.VITE_BACKEND_BASE_URL;
+
+export function addExerciseToDB(exercise: Exercise): Promise<Response> {
+  return fetch(`${BASE_URL}/exercise`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(exercise),
+  });
+}
+
+export function deleteExerciseFromDB(exerciseName: string): Promise<Response> {
+  return fetch(`${BASE_URL}/exercise/${encodeURIComponent(exerciseName)}`, {
+    method: "DELETE",
+  });
+}
+
+export function updateExerciseCountInDB(exercise: Exercise): Promise<Response> {
+  return fetch(`${BASE_URL}/exercise`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(exercise),
+  });
+}
+
+export async function fetchExercisesFromDB(): Promise<Exercise[]> {
+  const res = await fetch(`${BASE_URL}/exercises`);
+  return await res.json();
+}
